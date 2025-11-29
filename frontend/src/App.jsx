5a680de7535a4c17c8e12a0698fd3ab6a33afb7f
smritisingh <smritiiisinghh@gmail.com>
2025-11-29 09:04:44 +0530

    Backend and database setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Route, Router, RouterProvider} from "react-router"
+
+function App() {
+
+
+  return (
+
+    <RouterProvider>
+      <Router>
+        <Route></Route>
+      </Router>
+    </RouterProvider>
+  )
+}
+
+export default App
