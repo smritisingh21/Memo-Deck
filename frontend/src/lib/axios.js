3d26f1d4d3e96b37af288f0eecee6e3119c4c5d0
Smriti Singh <smritiiisinghh@gmail.com>
2026-01-06 16:32:09 +0530

    Changed base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://memo-deck.onrender.com/",
+  baseURL: "https://memo-deck.vercel.app/api/v1",
 });
-
-
 
 export default axiosInstance;
