b2256feb77c68e7bd79c619ae8fdac528bbb2d72
Smriti Singh <smritiiisinghh@gmail.com>
2026-01-06 16:50:27 +0530

    Added vercel origin in  server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4040;
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://memo-deck.vercel.app"],
   credentials: true,
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"]
 }));
